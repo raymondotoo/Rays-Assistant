b9bc17b47642c399b188f8bc1e74a018df6a8460
Raymond Otoo <raotoo@miners.utep.edu>
2026-03-13 11:40:41 -0700

    added app image

---
+++ electron-app/notarize.js
@@ -0,0 +1,21 @@
+require('dotenv').config();
+const { notarize } = require('@electron/notarize');
+
+exports.default = async function notarizing(context) {
+  const { electronPlatformName, appOutDir } = context;  
+  if (electronPlatformName !== 'darwin') {
+    return;
+  }
+
+  const appName = context.packager.appInfo.productFilename;
+  console.log(`  • notarizing ${appName} (this may take a while)...`);
+
+  return await notarize({
+    tool: 'notarytool',
+    appBundleId: 'com.rays.llm.dashboard',
+    appPath: `${appOutDir}/${appName}.app`,
+    appleId: process.env.APPLE_ID,
+    appleIdPassword: process.env.APPLE_APP_SPECIFIC_PASSWORD,
+    teamId: process.env.APPLE_TEAM_ID,
+  });
+};
